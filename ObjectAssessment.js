1265d534adc808d1f8c05c9b9e6b95746a15bb90
Carl Peaslee <poodle@Carls-MacBook-Pro.local>
2016-02-05 11:46:09 -0600

    first commit

---
+++ ObjectAssessment.js
@@ -0,0 +1,9 @@
+var avengersMovie = {
+	name : "Avengers",
+	runtimeMinutes : 143,
+	releaseYear : 2012,
+	Director : "Joss Whedon",
+	starring : ["Robert Downey Jr", "Chris Hemsworth", "Mark Ruffalo", "Scarlett Johansson"]
+};
+
+console.log(avengersMovie);
